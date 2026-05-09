3264e350826e76ff3d6b2637210e75391e19b956
nicholaskosakoski <nickk449@yahoo.com>
2026-05-09 16:02:55 -0700

    Refactor modals, fix formatting, add close icon and mobile styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,60 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const editProfileSubmitBtn = editProfileModal.querySelector(".modal__submit-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-const newPostSubmitBtn = newPostModal.querySelector(".modal__submit-btn");
 const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostCardImageInput = newPostModal.querySelector("#card-image-input");
+const newPostCardCaptionInput = newPostModal.querySelector("#card-caption-input");
 
-editProfileBtn.addEventListener("click" , function() {
-    editProfileModal.classList.add("modal_is-opened");
-})
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
-editProfileCloseBtn.addEventListener("click" , function() {
-    editProfileModal.classList.remove("modal_is-opened");
-})
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
 
-newPostBtn.addEventListener("click" , function() {
-    newPostModal.classList.add("modal_is-opened");
-})
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
 
-newPostCloseBtn.addEventListener("click" , function() {
-    newPostModal.classList.remove("modal_is-opened");
-})
+editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  openModal(editProfileModal);
+});
+
+editProfileCloseBtn.addEventListener("click", function () {
+  closeModal(editProfileModal);
+});
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+newPostBtn.addEventListener("click", function () {
+  openModal(newPostModal);
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  closeModal(newPostModal);
+});
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(newPostCardCaptionInput.value);
+  console.log(newPostCardImageInput.value);
+  closeModal(newPostModal);
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
